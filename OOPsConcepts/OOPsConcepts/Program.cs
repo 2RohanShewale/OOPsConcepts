d106aac1f54560046a7fc8e41e2dcd97ea3588e5
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-22 08:36:36 +0530

    [Rohan]Add: Object and Class Concept

---
+++ OOPsConcepts/OOPsConcepts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassName name = new ClassName();
+            name.PrintName();
+            //To Hold the screen until the key is pressed
+            Console.ReadKey();
+        }
+    }
+}
