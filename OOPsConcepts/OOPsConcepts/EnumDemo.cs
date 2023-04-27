fcf56a94205ebff50f1472daffa6fd9061871529
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-27 07:53:21 +0530

    [rohan]Add: Enumeration

---
+++ OOPsConcepts/OOPsConcepts/EnumDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcepts
+{
+    public class EnumDemo
+    {
+    }
+    enum Weekdays
+    {
+        Sunday =1,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
